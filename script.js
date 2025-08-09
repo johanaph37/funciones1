bb101b15d74531c2765fbc48579fdcd0c9ee7c90
johanaph37 <johanapenah@gmail.com>
2025-08-09 15:05:26 -0500

    función final: mostrar la factura completa, actualización de función

--- script.js
+++ script.js
@@ -100,3 +100,18 @@ function mostrarFactura() {
     console.log("--------------------------------");
     console.log("TOTAL A PAGAR:", total, "pesos \n");
 }
+let continuar = true;
+while (continuar) {
+    consumoGuardado = 0;
+    tarifaGuardada = 0;
+    cargoFijoGuardado = 0;
+    porcentajeIVAGuardado = 0;
+
+    mostrarFactura();
+
+    const respuesta = prompt("¿Deseas generar otra factura? Escribe si 'no', cualquier otra tecla para continuar:");
+
+    if (respuesta === "no" || respuesta === "n") {
+        continuar = false;
+    }
+}
